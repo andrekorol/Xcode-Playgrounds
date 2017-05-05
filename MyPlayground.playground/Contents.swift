fd1dff6fdc5d1d96fa3d790c6e520cede635816f
andrekorol <anrobits@yahoo.com.br>
2017-05-05 15:21:00 -0700

    Initial commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+
+let float1: Float = 56
+let float2: Float = 44
+let floatSummary = "I have \(float1 + float2)% of reason"
+
+let name = "Andre"
+print("Hello, \(name)")
+
+var shoppingList = ["catfish", "water", "tulips", "blue paint"]
+shoppingList[1] = "bottle of water"
+print(shoppingList)
+
+var occupations = [
+    "Malcolm": "Captain",
+    "Kaylee": "Mechanic",
+    
+]
+occupations["Jayne"] = "Public Relations"
+print(occupations)
+
+let emptyArray = [String]()
+let emptyDictionary = [String: Float]()
